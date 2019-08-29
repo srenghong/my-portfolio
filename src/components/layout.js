26bcb04515171b707f57822a8e6307e151ec601e
srenghong <srenghong21@gmail.com>
2019-08-29 10:39:01 +0200

    fix fontawesome flash very large icon

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { config } from '@fortawesome/fontawesome-svg-core';
 import '../styles/global.scss';
 
 import Header from './header';
 import Footer from './footer';
 import SEO from './SEO/seo';
+
+config.autoAddCss = false;
 
 const Layout = ({ children, className }) => (
   <div className="container">
